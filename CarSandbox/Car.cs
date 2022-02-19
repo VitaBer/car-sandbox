b6cc847f09f2bb1d4c2556491f0180f247877114
VitaBer <vita.berziunaite@gmail.com>
2022-02-19 17:38:31 +0200

    create initial commit

---
+++ CarSandbox/Car.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace sandbox
+{
+    public class Car
+    {
+        public string brandName;
+        public string modelName;
+        public int carDoorCount;
+        public int carBatteryCapacity;
+        public int mileageAmount;
+
+        public Car(string brandName, string modelName, int carDoorCount, int carBatteryCapacity, int mileageAmount)
+        {
+            this.brandName = brandName;
+            this.modelName = modelName;
+            this.carDoorCount = carDoorCount;
+            this.carBatteryCapacity = carBatteryCapacity;   
+            this.mileageAmount = mileageAmount;
+        }
+
+        public void increaseMilage(int changeby)
+        {
+            if (changeby > 0)
+            {
+                var newMileageAmount = mileageAmount + changeby;
+                mileageAmount = newMileageAmount;
+            }
+
+        }
+        public bool isNew ()
+        {
+            if(mileageAmount <=1000)
+            {
+                return true;
+            } else { return false; }
+        } 
+    }
+
+
+}
---
+++ CarSandbox/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+
+namespace sandbox
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            // See https://aka.ms/new-console-template for more information
+            Console.WriteLine("Hello, World!");
+            showCar();
+        }
+
+        public static void showCar()
+        {
+            var wwId3 = new Car("Volkswagen", "ID3", 4, 72, 0);
+            var peugot307 = new Car("Peugeot", "307", 2, 100, 1000);
+            var number = 7;
+            Console.WriteLine(wwId3.brandName);
+            Console.WriteLine(peugot307.brandName);
+
+            wwId3.mileageAmount = 10;
+            Console.WriteLine(wwId3.mileageAmount);
+
+            var changeby = 15;
+            wwId3.increaseMilage(changeby); // 'this' points to wwId3
+            //peugot307.increaseMilage(changeby); // 'this' points to peugeot307
+
+            //increaseMilage2(peugot307, changeby);
+            Console.WriteLine(wwId3.mileageAmount);
+
+            var carisNew = peugot307.isNew();
+            Console.WriteLine(peugot307.mileageAmount + " " + carisNew);
+            peugot307.increaseMilage(30);
+
+            carisNew = peugot307.isNew();
+            Console.WriteLine(peugot307.mileageAmount + " " + carisNew);
+
+
+        }
+
+
+        //public static void increaseMilage2(Car car, int changeby)
+        //{
+        //    car.mileageAmount = car.mileageAmount + changeby;
+
+        //}
+
+    }
+}
+
+
+
